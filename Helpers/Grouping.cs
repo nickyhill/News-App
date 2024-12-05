8c59cfd911332d047243846fd5ebf86f8f310bff
nickyhill <nhillengas@vt.edu>
2024-12-05 14:38:16 -0500

    Added hyperlinks for the urls and started implementing grouping in bookmarks

---
+++ Helpers/Grouping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberNewsApp.Helpers
+{
+    public class Grouping<TKey, TItem> : ObservableCollection<TItem>
+    {
+        public TKey Key { get; }
+
+        public Grouping(TKey key, IEnumerable<TItem> items) : base(items)
+        {
+            Key = key;
+        }
+    }
+}
